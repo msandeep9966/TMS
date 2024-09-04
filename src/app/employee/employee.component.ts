1ba81ea0f26b7ba763b463c0d757d7468123aebb
H4-MM-3R <kuheram777@gmail.com>
2024-09-04 14:04:50 +0530

    Third Commit

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { EmployeeService } from '../employee.service';
@@ -6,13 +6,16 @@ import { EmployeeService } from '../employee.service';
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgIf],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.scss',
 })
 export class EmployeeComponent {
   courses: any;
   enrolledCourses: any;
+  activeCourses: any;
+  pendingCourses: any;
+  approvedCourses: any;
   empidstring: any = sessionStorage.getItem('employeeId');
   mngidstring: any = sessionStorage.getItem('managerId');
   empid: any = parseInt(this.empidstring);
@@ -25,6 +28,12 @@ export class EmployeeComponent {
     this.o.getCoursesenrolled(this.empid).subscribe((data) => {
       this.enrolledCourses = data;
     });
+    this.o.getPendingCourses(this.empid).subscribe((data) => {
+      this.pendingCourses = data;
+    });
+    this.o.getActiveCourses(this.empid).subscribe((data) => {
+      this.activeCourses = data;
+    });
   }
 
   enroll(courseId: any) {
@@ -35,7 +44,19 @@ export class EmployeeComponent {
         managerId: this.mngid,
       })
       .subscribe((data) => {
-        alert(data)
+        this.o.getCoursesenrolled(this.empid).subscribe((data) => {
+          this.enrolledCourses = data;
+        });
+      });
+  }
+
+  cancel(enrollmentId: any) {
+    this.o
+      .cancelCourse({
+        enrollmentId: enrollmentId,
+        employeeId: this.empid,
+      })
+      .subscribe((data) => {
         this.o.getCoursesenrolled(this.empid).subscribe((data) => {
           this.enrolledCourses = data;
         });
